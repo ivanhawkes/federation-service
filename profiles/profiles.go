b2b5fdcca26978e6d35a7ccc7a59fe4df7884163
ivan <ivan.hawkes@gmail.com>
2013-10-20 00:15:09 +1000

    Added user CRUD

---
+++ profiles/profiles.go
@@ -0,0 +1,266 @@
+package profiles
+
+import (
+	"appengine"
+	"appengine/datastore"
+	"appengine/user"
+	"github.com/emicklei/go-restful"
+	"net/http"
+	"time"
+)
+
+// This example demonstrates a reasonably complete suite of RESTful operations backed
+// by DataStore on Google App Engine.
+
+// Our simple example struct.
+type Profile struct {
+	Id string `datastore:"-" json:"id" xml:"id"`
+	LastModified time.Time `json:"-" xml:"-"`
+	Email        string    `json:"-" xml:"-"`
+	FirstName    string    `json:"first_name" xml:"first-name"`
+	NickName     string    `json:"nick_name" xml:"nick-name"`
+	LastName     string    `json:"last_name" xml:"last-name"`
+}
+
+type ProfileApi struct {
+	Path string
+}
+
+func init() {
+	api := ProfileApi{Path: "/profiles"}
+	api.register()
+}
+
+// Register the routes we require for this resource type.
+//
+func (api ProfileApi) register() {
+	ws := new(restful.WebService)
+
+	ws.
+		Path(api.Path).
+		// You can specify consumes and produces per route as well.
+		Consumes(restful.MIME_JSON, restful.MIME_XML).
+		Produces(restful.MIME_JSON, restful.MIME_XML)
+
+	ws.Route(ws.POST("").To(api.create).
+		// Swagger documentation.
+		Doc("create a new profile").
+		Param(ws.BodyParameter("Profile", "representation of a profile").DataType("profiles.Profile")).
+		Reads(Profile{}))
+
+	ws.Route(ws.GET("/{profile-id}").To(api.read).
+		// Swagger documentation.
+		Doc("read a profile").
+		Param(ws.PathParameter("profile-id", "identifier for a profile").DataType("string")).
+		Writes(Profile{}))
+
+/*	ws.Route(ws.GET("/mine").To(api.mine).
+		// Swagger documentation.
+		Doc("return a list of all my profiles").
+		Writes(Profile{}))*/
+
+	ws.Route(ws.PUT("/{profile-id}").To(api.update).
+		// Swagger documentation.
+		Doc("update an existing profile").
+		Param(ws.PathParameter("profile-id", "identifier for a profile").DataType("string")).
+		Param(ws.BodyParameter("Profile", "representation of a profile").DataType("profiles.Profile")).
+		Reads(Profile{}))
+
+	ws.Route(ws.DELETE("/{profile-id}").To(api.delete).
+		// Swagger documentation.
+		Doc("delete a profile").
+		Param(ws.PathParameter("profile-id", "identifier for a profile").DataType("string")))
+
+	restful.Add(ws)
+}
+
+// Create a new resource.
+//
+func (api *ProfileApi) create(r *restful.Request, w *restful.Response) {
+	c := appengine.NewContext(r.Request)
+
+	// Marshall the entity from the request into a struct.
+	p := new(Profile)
+	err := r.ReadEntity(&p)
+	if err != nil {
+		w.WriteError(http.StatusNotAcceptable, err)
+		return
+	}
+
+	// Ensure we start with a sensible value for this field.
+	p.LastModified = time.Now()
+
+	// The resource belongs to this user.
+	p.Email = user.Current(c).String()
+
+	k, err := datastore.Put(c, datastore.NewIncompleteKey(c, "profiles", nil), p)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// The resource Id.
+	p.Id = k.Encode ()
+
+	// Let them know the location of the newly created resource.
+	// TODO: Use a safe Url path append function.
+	w.AddHeader("Location", api.Path+"/"+k.Encode())
+
+	// Return the resultant entity.
+	w.WriteHeader(http.StatusCreated)
+	w.WriteEntity(p)
+}
+
+// Read the resource.
+//
+func (api ProfileApi) read(r *restful.Request, w *restful.Response) {
+	c := appengine.NewContext(r.Request)
+
+	// Decode the request parameter to determine the key for the entity.
+	k, err := datastore.DecodeKey(r.PathParameter("profile-id"))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	// Retrieve the entity from the datastore.
+	p := Profile{}
+	if err := datastore.Get(c, k, &p); err != nil {
+		if err.Error() == "datastore: no such entity" {
+			http.Error(w, err.Error(), http.StatusNotFound)
+		} else {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+		return
+	}
+
+	// Check we own the resource before allowing them to view it.
+	// Optionally, return a 404 instead to help prevent guessing ids.
+	// TODO: Allow admins access.
+	if p.Email != user.Current(c).String() {
+		http.Error(w, "You do not have access to this resource", http.StatusForbidden)
+		return
+	}
+
+	// Set their Id.
+	p.Id = k.Encode ()
+	
+	w.WriteEntity(p)
+}
+
+// GET http://localhost:8080/profiles
+//
+/*
+func (api ProfileApi) mine(r *restful.Request, w *restful.Response) {
+	c := appengine.NewContext(r.Request)
+
+	// Find a list of all the profiles for the current user.
+	q := datastore.NewQuery("profiles").Filter("Email =", user.Current(c).String())
+
+	//
+	var profiles []Profile
+	if _, err := q.GetAll(c, &profiles); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Return the results.
+	w.WriteEntity(profiles)
+}*/
+
+// Update the resource.
+//
+func (api *ProfileApi) update(r *restful.Request, w *restful.Response) {
+	c := appengine.NewContext(r.Request)
+
+	// Decode the request parameter to determine the key for the entity.
+	k, err := datastore.DecodeKey(r.PathParameter("profile-id"))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	// Marshall the entity from the request into a struct.
+	p := new(Profile)
+	err = r.ReadEntity(&p)
+	if err != nil {
+		w.WriteError(http.StatusNotAcceptable, err)
+		return
+	}
+
+	// Retrieve the old entity from the datastore.
+	old := Profile{}
+	if err := datastore.Get(c, k, &old); err != nil {
+		if err.Error() == "datastore: no such entity" {
+			http.Error(w, err.Error(), http.StatusNotFound)
+		} else {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+		return
+	}
+
+	// Check we own the resource before allowing them to update it.
+	// Optionally, return a 404 instead to help prevent guessing ids.
+	// TODO: Allow admins access.
+	if old.Email != user.Current(c).String() {
+		http.Error(w, "You do not have access to this resource", http.StatusForbidden)
+		return
+	}
+
+	// Since the whole entity is re-written, we need to assign any invariant fields again
+	// e.g. the owner of the entity.
+	p.Email = user.Current(c).String()
+
+	// Keep track of the last modification date.
+	p.LastModified = time.Now()
+
+	// Attempt to overwrite the old entity.
+	_, err = datastore.Put(c, k, p)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Let them know it succeeded.
+	w.WriteHeader(http.StatusNoContent)
+}
+
+// Delete the resource.
+//
+func (api *ProfileApi) delete(r *restful.Request, w *restful.Response) {
+	c := appengine.NewContext(r.Request)
+
+	// Decode the request parameter to determine the key for the entity.
+	k, err := datastore.DecodeKey(r.PathParameter("profile-id"))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	// Retrieve the old entity from the datastore.
+	old := Profile{}
+	if err := datastore.Get(c, k, &old); err != nil {
+		if err.Error() == "datastore: no such entity" {
+			http.Error(w, err.Error(), http.StatusNotFound)
+		} else {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+		return
+	}
+
+	// Check we own the resource before allowing them to delete it.
+	// Optionally, return a 404 instead to help prevent guessing ids.
+	// TODO: Allow admins access.
+	if old.Email != user.Current(c).String() {
+		http.Error(w, "You do not have access to this resource", http.StatusForbidden)
+		return
+	}
+
+	// Delete the entity.
+	if err := datastore.Delete(c, k); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	// Success notification.
+	w.WriteHeader(http.StatusNoContent)
+}
---
+++ users/users.go
@@ -0,0 +1,251 @@
+package users
+
+import (
+	"appengine"
+	"appengine/datastore"
+	"appengine/user"
+	"github.com/emicklei/go-restful"
+	"net/http"
+	"time"
+)
+
+type UserShallow struct {
+	Id string `datastore:"-" json:"id" xml:"id"` // The Id for this resource.
+	DisplayName string `json:"display_name" xml:"display-name"`
+	AvatarUrl string `json:"avatar_url" xml:"avatar-url"`
+	Link string `datastore:"-" json:"link" xml:"link"`
+}
+
+type User struct {
+	UserShallow
+	OwnerId string `datastore:"OwnerId" json:"-" xml:"-"` // The external Id for the user who this represents. Comes from user authentication library.
+	LastModified time.Time `json:"-" xml:"-"`
+	Status int `json:"status" xml:"status"` // 1 - Pending activation, 2 - active, 3 - deactivated, 4 - deleted, 5 - permanently banned
+}
+
+type UserApi struct {
+	Path string
+}
+
+func init() {
+	api := UserApi{Path: "/users"}
+	api.register()
+}
+
+// Register the routes we require for this resource type.
+//
+func (api UserApi) register() {
+	ws := new(restful.WebService)
+
+	ws.
+		Path(api.Path).
+		// You can specify consumes and produces per route as well.
+		Consumes(restful.MIME_JSON, restful.MIME_XML).
+		Produces(restful.MIME_JSON, restful.MIME_XML)
+
+	ws.Route(ws.POST("").To(api.create).
+		// Swagger documentation.
+		Doc("create a new user").
+		Param(ws.BodyParameter("User", "representation of a user").DataType("users.User")).
+		Reads(User{}))
+
+	ws.Route(ws.GET("/{user-id}").To(api.read).
+		// Swagger documentation.
+		Doc("read a user").
+		Param(ws.PathParameter("user-id", "identifier for a user").DataType("string")).
+		Writes(User{}))
+
+	ws.Route(ws.PUT("/{user-id}").To(api.update).
+		// Swagger documentation.
+		Doc("update an existing user").
+		Param(ws.PathParameter("user-id", "identifier for a user").DataType("string")).
+		Param(ws.BodyParameter("User", "representation of a user").DataType("users.User")).
+		Reads(User{}))
+
+	ws.Route(ws.DELETE("/{user-id}").To(api.delete).
+		// Swagger documentation.
+		Doc("delete a user").
+		Param(ws.PathParameter("user-id", "identifier for a user").DataType("string")))
+
+	restful.Add(ws)
+}
+
+// Create a new resource.
+//
+func (api *UserApi) create(r *restful.Request, w *restful.Response) {
+	c := appengine.NewContext(r.Request)
+
+	// Marshall the entity from the request into a struct.
+	u := new(User)
+	err := r.ReadEntity(&u)
+	if err != nil {
+		w.WriteError(http.StatusNotAcceptable, err)
+		return
+	}
+
+	// Set some fields that need special handling.
+	u.LastModified = time.Now()
+	u.Status = 2
+
+	// The resource belongs to this user.
+	u.OwnerId = user.Current(c).ID
+
+	k, err := datastore.Put(c, datastore.NewIncompleteKey(c, "users", nil), u)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// The resource Id.
+	u.Id = k.Encode ()
+
+	// Let them know the location of the newly created resource.
+	// TODO: Use a safe Url path append function.
+	w.AddHeader("Location", api.Path+"/"+k.Encode())
+
+	// Provide a link for ease of API usage.
+	// TODO: This should be a fully qualified path.
+	u.Link = api.Path+"/"+k.Encode()
+
+	// Return the resultant entity.
+	w.WriteHeader(http.StatusCreated)
+	w.WriteEntity(u)
+}
+
+// Read the resource.
+//
+func (api UserApi) read(r *restful.Request, w *restful.Response) {
+	c := appengine.NewContext(r.Request)
+
+	// Decode the request parameter to determine the key for the entity.
+	k, err := datastore.DecodeKey(r.PathParameter("user-id"))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	// Retrieve the entity from the datastore.
+	u := User{}
+	if err := datastore.Get(c, k, &u); err != nil {
+		if err.Error() == "datastore: no such entity" {
+			http.Error(w, err.Error(), http.StatusNotFound)
+		} else {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+		return
+	}
+
+	// Check we own the resource before allowing them to view it.
+	// Optionally, return a 404 instead to help prevent guessing ids.
+	// TODO: Allow admins access.
+	//if u.Email != user.Current(c).String() {
+	//	http.Error(w, "You do not have access to this resource", http.StatusForbidden)
+	//	return
+	//}
+
+	// Set their Id.
+	u.Id = k.Encode ()
+
+	// Provide a link for ease of API usage.
+	// TODO: This should be a fully qualified path.
+	u.Link = api.Path+"/"+k.Encode()
+
+	w.WriteEntity(u)
+}
+
+// Update the resource.
+//
+func (api *UserApi) update(r *restful.Request, w *restful.Response) {
+	c := appengine.NewContext(r.Request)
+
+	// Decode the request parameter to determine the key for the entity.
+	k, err := datastore.DecodeKey(r.PathParameter("user-id"))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	// Marshall the entity from the request into a struct.
+	u := new(User)
+	err = r.ReadEntity(&u)
+	if err != nil {
+		w.WriteError(http.StatusNotAcceptable, err)
+		return
+	}
+
+	// Retrieve the old entity from the datastore.
+	old := User{}
+	if err := datastore.Get(c, k, &old); err != nil {
+		if err.Error() == "datastore: no such entity" {
+			http.Error(w, err.Error(), http.StatusNotFound)
+		} else {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+		return
+	}
+
+	// Check we own the resource before allowing them to update it.
+	// Optionally, return a 404 instead to help prevent guessing ids.
+	// TODO: Allow admins access.
+	if old.OwnerId != user.Current(c).ID {
+		http.Error(w, "You do not have access to this resource", http.StatusForbidden)
+		return
+	}
+
+	// Since the whole entity is re-written, we need to assign any invariant fields again
+	// e.g. the owner of the entity.
+	u.OwnerId = user.Current(c).ID
+
+	// Keep track of the last modification date.
+	u.LastModified = time.Now()
+
+	// Attempt to overwrite the old entity.
+	_, err = datastore.Put(c, k, u)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Let them know it succeeded.
+	w.WriteHeader(http.StatusNoContent)
+}
+
+// Delete the resource.
+//
+func (api *UserApi) delete(r *restful.Request, w *restful.Response) {
+	c := appengine.NewContext(r.Request)
+
+	// Decode the request parameter to determine the key for the entity.
+	k, err := datastore.DecodeKey(r.PathParameter("user-id"))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	// Retrieve the old entity from the datastore.
+	old := User{}
+	if err := datastore.Get(c, k, &old); err != nil {
+		if err.Error() == "datastore: no such entity" {
+			http.Error(w, err.Error(), http.StatusNotFound)
+		} else {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+		return
+	}
+
+	// Check we own the resource before allowing them to delete it.
+	// Optionally, return a 404 instead to help prevent guessing ids.
+	// TODO: Allow admins access.
+	if old.OwnerId != user.Current(c).ID {
+		http.Error(w, "You do not have access to this resource", http.StatusForbidden)
+		return
+	}
+
+	// Delete the entity.
+	if err := datastore.Delete(c, k); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	// Success notification.
+	w.WriteHeader(http.StatusNoContent)
+}
