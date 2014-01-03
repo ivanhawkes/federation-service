d6728f8034003e9c5933f949e524ab5c08acf2f4
ivan <ivan.hawkes@gmail.com>
2014-01-03 11:15:40 +1000

    Added loot table.

--- loottable/loottable.go
+++ loottable/loottable.go
@@ -1,9 +1,8 @@
-package federations
+package loottable
 
 import (
 	"appengine"
 	"appengine/datastore"
-	"appengine/user"
 	"github.com/emicklei/go-restful"
 	"log"
 	"net/http"
@@ -11,10 +10,10 @@ import (
 )
 
 const (
-	rootPath = "/federations"
+	rootPath = "/loottable"
 )
 
-// The various states for a federation resource.
+// The various states for a loottable resource.
 const (
 	StatusActive = iota
 	StatusDeactivated
@@ -23,30 +22,29 @@ const (
 	StatusDeleted
 )
 
-type FederationShallow struct {
+type LootShallow struct {
 	Id   string `datastore:"-" json:"id" xml:"id"`
 	Name string `json:"name" xml:"name"`
 	Link string `datastore:"-" json:"link" xml:"link"`
 }
 
-type Federation struct {
-	FederationShallow
-	UserId       string    `datastore:"UserId" json:"-" xml:"-"`
+type Loottable struct {
+	LootShallow
 	LastModified time.Time `json:"-" xml:"-"`
 	Status       int       `json:"status" xml:"status"`
 }
 
-type FederationApi struct {
+type LoottableApi struct {
 	Path string
 }
 
 func init() {
-	log.Printf("Federations: Register")
+	log.Printf("Loottable: Register")
 }
 
 // Register the routes we require for this resource type.
 //
-func (api FederationApi) Register() {
+func (api LoottableApi) Register() {
 	ws := new(restful.WebService)
 
 	ws.
@@ -56,76 +54,77 @@ func (api FederationApi) Register() {
 
 	ws.Route(ws.POST("").To(api.create).
 		// Swagger documentation.
-		Doc("create a new federation").
-		Param(ws.BodyParameter("Federation", "representation of a federation").DataType("federations.Federation")).
-		Reads(Federation{}))
+		Doc("create a new loottable").
+		Param(ws.BodyParameter("Loottable", "representation of a loottable").DataType("loottable.Loottable")).
+		Reads(Loottable{}))
 
-	ws.Route(ws.GET("/{federation-id}").To(api.read).
+	ws.Route(ws.GET("/{loottable-id}").To(api.read).
 		// Swagger documentation.
-		Doc("read a federation").
-		Param(ws.PathParameter("federation-id", "identifier for a federation").DataType("string")).
-		Writes(Federation{}))
+		Doc("read a loottable").
+		Param(ws.PathParameter("loottable-id", "identifier for a loottable").DataType("string")).
+		Writes(Loottable{}))
 
-	ws.Route(ws.PUT("/{federation-id}").To(api.update).
+	ws.Route(ws.PUT("/{loottable-id}").To(api.update).
 		// Swagger documentation.
-		Doc("update an existing federation").
-		Param(ws.PathParameter("federation-id", "identifier for a federation").DataType("string")).
-		Param(ws.BodyParameter("Federation", "representation of a federation").DataType("federations.Federation")).
-		Reads(Federation{}))
+		Doc("update an existing loottable").
+		Param(ws.PathParameter("loottable-id", "identifier for a loottable").DataType("string")).
+		Param(ws.BodyParameter("Loottable", "representation of a loottable").DataType("loottable.Loottable")).
+		Reads(Loottable{}))
 
-	ws.Route(ws.DELETE("/{federation-id}").To(api.delete).
+	ws.Route(ws.DELETE("/{loottable-id}").To(api.delete).
 		// Swagger documentation.
-		Doc("delete a federation").
-		Param(ws.PathParameter("federation-id", "identifier for a federation").DataType("string")))
+		Doc("delete a loottable").
+		Param(ws.PathParameter("loottable-id", "identifier for a loottable").DataType("string")))
 
 	restful.Add(ws)
 }
 
 // Create a new resource.
 //
-func (api *FederationApi) create(r *restful.Request, w *restful.Response) {
+func (api *LoottableApi) create(r *restful.Request, w *restful.Response) {
 	c := appengine.NewContext(r.Request)
 
 	// Marshall the entity from the request into a struct.
-	f := new(Federation)
-	err := r.ReadEntity(&f)
+	loottable := new(Loottable)
+	err := r.ReadEntity(&loottable)
 	if err != nil {
 		w.WriteError(http.StatusNotAcceptable, err)
 		return
 	}
 
 	// Set some fields that need special handling.
-	f.LastModified = time.Now()
-	f.Status = StatusActive
+	loottable.LastModified = time.Now()
+	loottable.Status = StatusActive
 
-	// The resource belongs to this federation.
-	f.UserId = user.Current(c).ID
+	// The resource belongs to this loottable.
+	//loottable.UserId = user.Current(c).ID
 
+	// TODO: Should be ancestor to a federation.
 	// Set a user as our ancestor...this is done by querying for the key for the current user.
-	var ancestor *datastore.Key
-	q := datastore.NewQuery("users").
-		Filter("UserId =", user.Current(c).ID).
-		KeysOnly()
-	if keys, err := q.GetAll(c, nil); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	} else {
-		if keys == nil {
-			http.Error(w, "There is no user resource for this login account", http.StatusNotAcceptable)
+	/*	var ancestor *datastore.Key
+		q := datastore.NewQuery("users").
+			Filter("UserId =", user.Current(c).ID).
+			KeysOnly()
+		if keys, err := q.GetAll(c, nil); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
-		}
-		ancestor = keys[0]
-	}
+		} else {
+			if keys == nil {
+				http.Error(w, "There is no user resource for this login account", http.StatusNotAcceptable)
+				return
+			}
+			ancestor = keys[0]
+		}*/
 
-	// Store the federation.
-	k, err := datastore.Put(c, datastore.NewIncompleteKey(c, "federations", ancestor), f)
+	// Store the loottable.
+	k, err := datastore.Put(c, datastore.NewIncompleteKey(c, "loottable", nil /*ancestor*/), loottable)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
 	// The resource Id.
-	f.Id = k.Encode()
+	loottable.Id = k.Encode()
 
 	// Let them know the location of the newly created resource.
 	// TODO: Use a safe Url path append function.
@@ -133,28 +132,28 @@ func (api *FederationApi) create(r *restful.Request, w *restful.Response) {
 
 	// Provide a link for ease of API usage.
 	// TODO: This should be a fully qualified path.
-	f.Link = rootPath + "/" + k.Encode()
+	loottable.Link = rootPath + "/" + k.Encode()
 
 	// Return the resultant entity.
 	w.WriteHeader(http.StatusCreated)
-	w.WriteEntity(f)
+	w.WriteEntity(loottable)
 }
 
 // Read the resource.
 //
-func (api FederationApi) read(r *restful.Request, w *restful.Response) {
+func (api LoottableApi) read(r *restful.Request, w *restful.Response) {
 	c := appengine.NewContext(r.Request)
 
 	// Decode the request parameter to determine the key for the entity.
-	k, err := datastore.DecodeKey(r.PathParameter("federation-id"))
+	k, err := datastore.DecodeKey(r.PathParameter("loottable-id"))
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
 	// Retrieve the entity from the datastore.
-	f := Federation{}
-	if err := datastore.Get(c, k, &f); err != nil {
+	loottable := Loottable{}
+	if err := datastore.Get(c, k, &loottable); err != nil {
 		if err.Error() == "datastore: no such entity" {
 			http.Error(w, err.Error(), http.StatusNotFound)
 		} else {
@@ -166,43 +165,43 @@ func (api FederationApi) read(r *restful.Request, w *restful.Response) {
 	// Check we own the resource before allowing them to view it.
 	// Optionally, return a 404 instead to help prevent guessing ids.
 	// TODO: Allow admins access.
-	//if f.UserId != user.Current(c).ID {
+	//if loottable.UserId != user.Current(c).ID {
 	//	http.Error(w, "You do not have access to this resource", http.StatusForbidden)
 	//	return
 	//}
 
 	// Set their Id.
-	f.Id = k.Encode()
+	loottable.Id = k.Encode()
 
 	// Provide a link for ease of API usage.
 	// TODO: This should be a fully qualified path.
-	f.Link = rootPath + "/" + k.Encode()
+	loottable.Link = rootPath + "/" + k.Encode()
 
-	w.WriteEntity(f)
+	w.WriteEntity(loottable)
 }
 
 // Update the resource.
 //
-func (api *FederationApi) update(r *restful.Request, w *restful.Response) {
+func (api *LoottableApi) update(r *restful.Request, w *restful.Response) {
 	c := appengine.NewContext(r.Request)
 
 	// Decode the request parameter to determine the key for the entity.
-	k, err := datastore.DecodeKey(r.PathParameter("federation-id"))
+	k, err := datastore.DecodeKey(r.PathParameter("loottable-id"))
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
 	// Marshall the entity from the request into a struct.
-	f := new(Federation)
-	err = r.ReadEntity(&f)
+	loottable := new(Loottable)
+	err = r.ReadEntity(&loottable)
 	if err != nil {
 		w.WriteError(http.StatusNotAcceptable, err)
 		return
 	}
 
 	// Retrieve the old entity from the datastore.
-	old := Federation{}
+	old := Loottable{}
 	if err := datastore.Get(c, k, &old); err != nil {
 		if err.Error() == "datastore: no such entity" {
 			http.Error(w, err.Error(), http.StatusNotFound)
@@ -215,20 +214,20 @@ func (api *FederationApi) update(r *restful.Request, w *restful.Response) {
 	// Check we own the resource before allowing them to update it.
 	// Optionally, return a 404 instead to help prevent guessing ids.
 	// TODO: Allow admins access.
-	if old.UserId != user.Current(c).ID {
-		http.Error(w, "You do not have access to this resource", http.StatusForbidden)
-		return
-	}
+	// if old.UserId != user.Current(c).ID {
+	// 	http.Error(w, "You do not have access to this resource", http.StatusForbidden)
+	// 	return
+	// }
 
 	// Since the whole entity is re-written, we need to assign any invariant fields again
 	// e.g. the owner of the entity.
-	f.UserId = user.Current(c).ID
+	// loottable.UserId = user.Current(c).ID
 
 	// Keep track of the last modification date.
-	f.LastModified = time.Now()
+	loottable.LastModified = time.Now()
 
 	// Attempt to overwrite the old entity.
-	_, err = datastore.Put(c, k, f)
+	_, err = datastore.Put(c, k, loottable)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
@@ -240,18 +239,18 @@ func (api *FederationApi) update(r *restful.Request, w *restful.Response) {
 
 // Delete the resource.
 //
-func (api *FederationApi) delete(r *restful.Request, w *restful.Response) {
+func (api *LoottableApi) delete(r *restful.Request, w *restful.Response) {
 	c := appengine.NewContext(r.Request)
 
 	// Decode the request parameter to determine the key for the entity.
-	k, err := datastore.DecodeKey(r.PathParameter("federation-id"))
+	k, err := datastore.DecodeKey(r.PathParameter("loottable-id"))
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
 	// Retrieve the old entity from the datastore.
-	old := Federation{}
+	old := Loottable{}
 	if err := datastore.Get(c, k, &old); err != nil {
 		if err.Error() == "datastore: no such entity" {
 			http.Error(w, err.Error(), http.StatusNotFound)
@@ -264,10 +263,10 @@ func (api *FederationApi) delete(r *restful.Request, w *restful.Response) {
 	// Check we own the resource before allowing them to delete it.
 	// Optionally, return a 404 instead to help prevent guessing ids.
 	// TODO: Allow admins access.
-	if old.UserId != user.Current(c).ID {
-		http.Error(w, "You do not have access to this resource", http.StatusForbidden)
-		return
-	}
+	// if old.UserId != user.Current(c).ID {
+	// 	http.Error(w, "You do not have access to this resource", http.StatusForbidden)
+	// 	return
+	// }
 
 	// Delete the entity.
 	if err := datastore.Delete(c, k); err != nil {
--- main.go
+++ main.go
@@ -3,22 +3,18 @@ package main
 import (
 	"characters"
 	"factions"
-	"federations"
-	"my"
-	"profiles"
 	"realms"
 	"users"
 	"zones"
+	"loottable"
 )
 
 func init() {
 	// Register all the routes we need and document the interface.
 	characters.CharacterApi{}.Register()
 	factions.FactionApi{}.Register()
-	federations.FederationApi{}.Register()
-	my.MyApi{}.Register()
-	profiles.ProfileApi{}.Register()
 	realms.RealmApi{}.Register()
 	users.UserApi{}.Register()
 	zones.ZoneApi{}.Register()
+	loottable.LoottableApi{}.Register()
 }
