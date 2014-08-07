bc6f64bb1837799605f5e11e415fa3ed069a3520
ivan <ivan.hawkes@gmail.com>
2014-08-07 19:29:37 +1000

    running, needs testing

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"accounts"
 	"appengine"
 	"appengine/user"
 	"buffs"
@@ -11,6 +12,7 @@ import (
 	"factions"
 	"fmt"
 	"github.com/emicklei/go-restful"
+	"github.com/emicklei/go-restful/swagger"
 	"items"
 	"itemtemplates"
 	"loottables"
@@ -22,8 +24,21 @@ import (
 	"storagecontainers"
 	"storageitems"
 	"users"
+	"verbs"
 	"zones"
 )
+
+func gaeUrl() string {
+	if appengine.IsDevAppServer() {
+		//		return "http://localhost:8080"
+		return "http://localhost:8080"
+	} else {
+		// Include your URL on App Engine here.
+		// I found no way to get AppID without appengine.Context and that is always
+		// based on a http.Request.
+		return "http://api.shatteredscreens.com"
+	}
+}
 
 func init() {
 	// Enable CORS on the default container.
@@ -38,6 +53,7 @@ func init() {
 	//wsContainer.Filter(wsContainer.OPTIONSFilter)
 
 	// Register all the routes we need and document the interface.
+	accounts.Register()
 	characters.Register()
 	factions.Register()
 	profiles.Register()
@@ -61,6 +77,70 @@ func init() {
 
 	// Test page.
 	registerMyTest()
+
+	// Register the verbs.
+	verbs.RegisterVerbRegister()
+
+	// Optionally, you can install the Swagger Service which provides a nice Web UI on your REST API
+	// You need to download the Swagger HTML5 assets and change the FilePath location in the config below.
+	// Open <your_app_id>.appspot.com/apidocs and enter
+	// http://<your_app_id>.appspot.com/apidocs.json in the api input field.
+	config := swagger.Config{
+		// You control what services are visible
+		WebServices:    restful.RegisteredWebServices(),
+		WebServicesUrl: gaeUrl(),
+		ApiPath:        "/apidocs.json",
+
+		// Optionally, specifiy where the UI is located
+		SwaggerPath: "/apidoc/",
+
+		// GAE support static content which is configured in your app.yaml.
+		// This example expect the swagger-ui in static/swagger so you should place it there :)
+		SwaggerFilePath: "static/swagger/"}
+	swagger.InstallSwaggerService(config)
+
+}
+
+func registerLoginRequired() {
+	ws := new(restful.WebService)
+	ws.Path("/_ah")
+	ws.Route(ws.GET("/login_required").To(loginRequired).
+		Doc("Federated user login page").
+		Operation("Federated user login page"))
+	restful.Add(ws)
+}
+
+// Present a page for federated logins.
+//
+func loginRequired(r *restful.Request, w *restful.Response) {
+	c := appengine.NewContext(r.Request)
+
+	providers := map[string]string{
+		"Google": "www.google.com/accounts/o8/id",
+		"Steam":  "steamcommunity.com/openid", // no email with this one
+		"Yahoo":  "yahoo.com",
+		"AOL":    "aol.com",
+	}
+
+	// Provide support for redirection back to the page they came from.
+	cont := r.QueryParameter("continue")
+	if len(cont) == 0 {
+		cont = "/"
+	}
+
+	// Header.
+	fmt.Fprintf(w, "<html><head></head><body>")
+
+	for name, url := range providers {
+		login_url, err := user.LoginURLFederated(c, cont, url)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Fprintf(w, "<a href='%s'>%s</a><br>", login_url, name)
+	}
+
+	// Footer.
+	fmt.Fprintf(w, "</body></html>")
 }
 
 func registerMyTest() {
@@ -72,7 +152,7 @@ func registerMyTest() {
 	restful.Add(ws)
 }
 
-// Present a page for federated logins.
+// Test page.
 //
 func myTestGet(r *restful.Request, w *restful.Response) {
 	c := appengine.NewContext(r.Request)
---
+++ verbs/register.go
@@ -0,0 +1,135 @@
+package verbs
+
+import (
+	"accounts"
+	"appengine"
+	"appengine/datastore"
+	"github.com/emicklei/go-restful"
+	"log"
+	"net/http"
+	"resource"
+	"strconv"
+	"time"
+)
+
+type RegisterResource struct {
+	Email string `json:"email" xml:"email"`
+}
+
+type RegisterRequest struct {
+	RegisterResource
+}
+
+type RegisterResponse struct {
+	resource.Meta
+	RegisterResource
+}
+
+func init() {
+}
+
+// Register the verb.
+//
+func RegisterVerbRegister() {
+	log.Printf("VERB: /register")
+
+	ws := new(restful.WebService)
+
+	ws.
+		Path("/register").
+		Consumes(restful.MIME_JSON, restful.MIME_XML).
+		Produces(restful.MIME_JSON, restful.MIME_XML)
+
+	ws.Route(ws.POST("").To(postRegister).
+		Doc("Register a new account").
+		Operation("verbRegister").
+		Param(ws.BodyParameter("Accounts.Resource", "representation of a resource").DataType("Accounts.Resource")).
+		Reads(RegisterRequest{}).
+		Writes(RegisterResponse{}))
+
+	restful.Add(ws)
+}
+
+// Create a new resource.
+//
+func postRegister(r *restful.Request, w *restful.Response) {
+	c := appengine.NewContext(r.Request)
+
+	// Marshall the entity from the request into a struct.
+	res := new(RegisterRequest)
+	err := r.ReadEntity(res)
+	if err != nil {
+		w.AddHeader("Content-Type", "text/plain")
+		w.WriteErrorString(http.StatusNotAcceptable, err.Error())
+		return
+	}
+
+	// Create a new account based on the limited information from registration.
+	acc := new(accounts.ResourceMeta)
+
+	// Marshall the entity from the request into a struct.
+	resp := new(RegisterResponse)
+	err = r.ReadEntity(resp)
+	if err != nil {
+		resource.WriteError(w, resource.NewError(http.StatusNotAcceptable, "/html/error/statusnotacceptable", err.Error()))
+		return
+	}
+
+	// TODO: don't allow them to register the same email or ID twice
+
+	// Set the meta data for the resource.
+	resp.LastModified = time.Now()
+	resp.Status = accounts.StatusActive
+	resp.Revision = 1
+
+	// TODO: we want the email address and ID to come from OAuth
+
+	// Grab their email address.
+	//	resp.Email = user.Current(c).Email
+
+	// Copy over the information we have.
+	acc.LastModified = resp.LastModified
+	acc.Status = resp.Status
+	acc.Revision = resp.Revision
+	acc.AccessLevel = accounts.AccessLevelGeneral
+
+	// Snag the information provided by OpenID  OAuth.
+	acc.Email = resp.Email
+	// acc.OpenId = user.Current(c).ID
+	// acc.AuthDomain = user.Current(c).AuthDomain
+	// acc.FederatedIdentity = user.Current(c).FederatedIdentity
+	// acc.FederatedProvider = user.Current(c).FederatedProvider
+
+	// If we don't have a valid email address for them, mark the account as not yet active.
+	// TODO: need to send them an email validation - work out where in the process to add this.
+	if len(acc.Email) == 0 {
+		acc.Status = accounts.StatusActivationPending
+	}
+
+	// Store the resource.
+	k, err := datastore.Put(c, datastore.NewIncompleteKey(c, "accounts", nil), acc)
+	if err != nil {
+		w.AddHeader("Content-Type", "text/plain")
+		w.WriteErrorString(http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	// The resource Key.
+	resp.Key = *k
+
+	// Let them know the location of the newly created resource.
+	// TODO: Use a safe Url path append function.
+	w.AddHeader("Location", accounts.PreferredLink(k))
+
+	// Provide a link for ease of API usage.
+	resp.Link.Rel = "self"
+	resp.Link.Href = accounts.PreferredLink(k)
+
+	// Set the headers.
+	w.WriteHeader(http.StatusCreated)
+	w.AddHeader(restful.HEADER_LastModified, resp.LastModified.Format(time.RFC3339Nano))
+	w.AddHeader("ETag", strconv.Itoa(resp.Revision))
+
+	// Output the response body.
+	w.WriteEntity(resp)
+}
